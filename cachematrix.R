71471e1463eccec82169b2be929cc60b62f3270c
tommie chen <tochen@hotmail.com>
2016-02-16 15:56:27 -0500

    Adding codes to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# ------------------------------------------------------------------------
+#   R Programming - Assignment #2
+#   (1) makeCacheMatrix: a function that creates a list of sub-functions: set, get, setinv and getinv
+#   (2) input of makeCacheMatrix is a matrix
+#   (3) cacheSolve: a function that computes the inverse of the special "matrix" returned by makeCacheMatrix above
+#   (4) please see the last section of this program to test run it
+# ------------------------------------------------------------------------
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get, setinv= setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  m <- x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
 }
+
+# this is how we test the program:
+m <- matrix(c(1,2,3,4), nrow = 2, ncol = 2, byrow = FALSE) # declare a matrix
+m_test_inv <- makeMatrix(m)
+cacheSolve(m_test_inv) # output  
+
+# this is how we compare above result with R's generic function "solve":
+X <- matrix(c(1,2,3,4), nrow = 2, ncol = 2, byrow = FALSE)
+solve(X) # output
